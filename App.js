345a2cc0ddfeedd92a49138169a1cb0c562782ef
saad <saad@socar.my>
2020-09-11 22:03:43 +0800

    uploaded

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { firebase } from './src/firebase/config'
 
 export default function App() {
+  React.useEffect(() => {
+    firebase
+      .auth().createUserWithEmailAndPassword("saad@tst.com", "password")
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
